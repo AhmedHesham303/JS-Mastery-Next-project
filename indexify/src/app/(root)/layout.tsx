0bb2e7e6740509d1ef40ccd54e048afb2fa563d0
AhmedHesham <ahmed.hesham303303@gmail.com>
2026-01-18 11:19:27 +0200

    Added auth.js and githup as a provider

---
+++ indexify/src/app/(root)/layout.tsx
@@ -0,0 +1,13 @@
+import type { ReactNode } from "react";
+import Navbar from "../components/Navbar";
+
+export default function layout({
+  children,
+}: Readonly<{ children: ReactNode }>) {
+  return (
+    <main className="font-work-sans">
+      <Navbar />
+      {children}
+    </main>
+  );
+}
